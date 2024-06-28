28b9a8cfed157f71e8c1ab64c8e9b453ff1a2b56
Gitanshu <95081115+gitanshu-nrt@users.noreply.github.com>
2024-06-28 19:02:24 +0530

    chore: Update navbar and background color styles
    - Update the navbar in the ProductList component to display the Monk Upsell & Cross-sell logo and brand name.
    - Adjust the background color in the index.css file to #f6f6f8.

--- src/Product/ProductList.js
+++ src/Product/ProductList.js
@@ -3,22 +3,22 @@ import React, { useState } from "react";
 import Logo from "./image.png";
 
 const ProductList = () => {
-  return (
-    <>
-      <Navbar fluid rounded className='hidden md:block border-2 border-brand bg-brand'>
-        <Navbar.Brand>
-          <img
-            src={Logo}
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite React Logo"
-          />
-          <span className="self-center text-xl font-semibold text-gray-500">
-          Monk Upsell & Cross-sell
-          </span>
-        </Navbar.Brand>
-       
-      </Navbar>
-      {/* <Navbar fluid rounded className='hidden md:block border-2 border-brand bg-brand'>
+    return (
+        <>
+            <Navbar fluid rounded className='hidden md:block border-2 border-brand bg-brand'>
+                <Navbar.Brand>
+                    <img
+                        src={Logo}
+                        className="mr-3 h-6 sm:h-9"
+                        alt="Flowbite React Logo"
+                    />
+                    <span className="self-center text-xl font-semibold text-gray-50">
+                        Monk Upsell & Cross-sell
+                    </span>
+                </Navbar.Brand>
+
+            </Navbar>
+            {/* <Navbar fluid rounded className='hidden md:block border-2 border-brand bg-brand'>
                 <Navbar.Brand className='flex-col' href="">
 
                 <img src={Logo} />   <span className="flex self-center whitespace-nowrap text-xl font-semibold text-red"> GYIZER </span>
@@ -27,16 +27,17 @@ const ProductList = () => {
                
 
             </Navbar> */}
-            
-        <div className="mt-8 ml-4">
-            <h1>Add Products</h1>
+<div className="flex mt-4">
+            <div className=" pl-80 ">
+                <h1>Add Products</h1>
             </div>
 
-            <div className="mt-4 ml-4">
+            <div className="ml-9">
                 Product
             </div>
-    </>
-  );
+            </div>
+        </>
+    );
 };
 
 export default ProductList;
